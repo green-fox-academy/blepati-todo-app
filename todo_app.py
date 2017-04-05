909b7b38121df3d733ccd9cd71e475a5d18e5641
Blesa Patricia <blesapati@gmail.com>
2017-04-05 22:06:36 +0200

    feat(print) add usage description to the app

---
+++ todo_app.py
@@ -0,0 +1,8 @@
+print("Python Todo application")
+print("=======================")
+
+print("Command line arguments:")
+print(" -l   Lists all the tasks")
+print(" -a   Adds a new task")
+print(" -r   Removes an task")
+print(" -c   Completes an task")
