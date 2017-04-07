5e46e8406dbfff360c64953504dcf166c45cb246
Blesa Patricia <blesapati@gmail.com>
2017-04-07 14:18:35 +0200

    feat(task remove) now the task remover function works properly yet

--- todo_app.py
+++ todo_app.py
@@ -2,8 +2,25 @@ import sys
 
 #print(sys.argv)
 
+def print_help():
+    help_content = ["Python Todo application",
+                    "=======================",
+                    "Command line arguments:",
+                    " -l   Lists all the tasks",
+                    " -a   Adds a new task, note: your task need to be inside double quotemarks",
+                    " -r   Removes an task",
+                    " -c   Completes an task"]
+
+    for content in help_content:
+        print(content)
+
+#def read_todos():
+    #todo_file = open("todo_data.txt", "r")
+    #todos = todo_file.readlines()
+    #todo_file.close()
+
 def print_todos():
-    todo_file = open("todo_data.txt")
+    todo_file = open("todo_data.txt", "r")
     todos = todo_file.readlines()
     todo_file.close()
     if len(todos) == 0:
@@ -11,19 +28,6 @@ def print_todos():
     else:
         for todo in range(1, len(todos) + 1):
             print(str(todo) + " - " + todos[todo - 1].rstrip())#leszedi a white spacet a txt file sorai vegerol
-
-
-def print_help():
-    help_content = ["Python Todo application",
-                    "=======================",
-                    "Command line arguments:",
-                    " -l   Lists all the tasks",
-                    " -a   Adds a new task",
-                    " -r   Removes an task",
-                    " -c   Completes an task"]
-
-    for content in help_content:
-        print(content)
 
 def add_todo():
     todo_file = open("todo_data.txt", "a")
@@ -35,12 +39,26 @@ def add_todo():
     else:
         print("you did not add task yet")
 
+def delete_todo():
+    todo_file = open("todo_data.txt", "r")
+    todos = todo_file.readlines()
+    todo_file.close()
+    remove_todos = open("todo_data.txt", "w")
+    remove_todo_num = int(sys.argv[2])
+    for todo in range(len(todos)):
+        if todo == remove_todo_num - 1:
+            pass
+        else:
+            remove_todos.write(todos[todo])
+
 def controller(arg):
     if len(sys.argv) >= 2:
         if sys.argv[1] == "-l":
             print_todos()
         elif sys.argv[1] == "-a":
             add_todo()
+        elif sys.argv[1] == "-r":
+            delete_todo()
     else:
         print_help()
 
